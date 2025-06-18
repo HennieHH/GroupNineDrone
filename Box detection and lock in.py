f0f83553093e00dea9831a304f027a85670b060b
HennieHH <hendrikhellinga@gmail.com>
2025-06-18 13:29:34 +0200

    Added magnet turing on or off based on object detection

--- Box detection and lock in.py
+++ Box detection and lock in.py
@@ -2,15 +2,20 @@ from machine import Pin
 import time
 
 
-class ObstacleDetector:
+class IR_Magnet:
     def __init__(self):
+        # IR Sensor Setup
         self.sensor = Pin(16, Pin.IN, Pin.PULL_UP)
+        # Magnet Setup
+        self.magnet = Pin(26, Pin.OUT)
+        self.magnet.off()  # Start with magnet off
+
         self.detection_start = None
         self.triggered = False
         self.clear_count = 0
         self.REQUIRED_CLEAR_CHECKS = 10  # Number of clear readings needed to reset
 
-    def check_4_second_obstacle(self):
+    def obstacle_detection(self):
         current_state = not self.sensor.value()  # True when obstacle detected
 
         if current_state:
@@ -18,8 +23,10 @@ class ObstacleDetector:
 
             if self.detection_start is None:
                 self.detection_start = time.time()
-            elif not self.triggered and (time.time() - self.detection_start >= 4):
+            elif not self.triggered and (time.time() - self.detection_start >= 1):  # 1 second detection time
                 self.triggered = True
+                self.magnet.on()
+                print("Continuous obstacle detected for 1+ second! Magnet ON")
                 return True
         else:
             self.clear_count += 1
@@ -28,19 +35,16 @@ class ObstacleDetector:
             if self.clear_count >= self.REQUIRED_CLEAR_CHECKS:
                 self.detection_start = None
                 self.triggered = False
+                self.magnet.off()
                 self.clear_count = 0
-                print("No object detected ")
+                print("No object detected, Magnet OFF")
 
         return False
 
 
 # Usage:
-detector = ObstacleDetector()  # Use your actual pin number
+detector = IR_Magnet()  # Create instance
 
 while True:
-    if detector.check_4_second_obstacle():
-        print("Continuous obstacle detected for 4+ seconds!")
-        # Add your response actions here
-
-    # Rest of your program runs here
+    detector.obstacle_detection()  # Removed colon after this line
     time.sleep(0.1)  # 100ms between checks
